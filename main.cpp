6142f1e3c35e10694d59d52e980885d52b15600e
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2024-12-03 15:28:18 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include "BoardGame_Classes.h"
+#include " Pyramic_Tic-Tac-Toe.h"
+
+using namespace std;
+
+void game1() {
+
+    char choice;
+    Player<char> *players[2];
+    pyramid<char> *B = new pyramid<char>();
+    string playerXName, player2Name;
+
+
+    // Set up player 1
+    cout << "Enter Player X name: ";
+    cin >> playerXName;
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    while (true) {
+        cin >> choice;
+
+        if (choice == '1') {
+            players[0] = new X_O_Player<char>(playerXName, 'X');
+            break;
+        } else if (choice == '2') {
+            players[0] = new X_O_Random_Player<char>('X');
+            break;
+        } else {
+            cout << "Invalid choice for Player 1. enter valid choice \n";
+        }
+    }
+    // Set up player 2
+    cout << "Enter Player 2 name: ";
+    cin >> player2Name;
+    cout << "Choose Player 2 type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+
+    while (true) {
+        cin >> choice;
+
+        if (choice == '1') {
+            players[1] = new X_O_Player<char>(player2Name, 'O');
+            break;
+        } else if (choice == '2') {
+            players[1] = new X_O_Random_Player<char>('O');
+            break;
+        } else {
+            cout << "Invalid choice for Player 2. enter valid choice.\n";
+        }
+    }
+
+    // Create the game manager and run the game
+    GameManager<char> pyramid_game(B, players);
+    pyramid_game.run();
+
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+
+}
+
+int main() {
+
+    char n;
+    cout << "Welcome To you , let's We Play " << endl;
+    cout << "-------------------------------" << endl;
+    cout << "Choice a number " << endl;
+    cout << "----------------" << endl;
+    cout << "0 - exit " << endl;
+    cout << "1- Pyramid Tic-Tac-Toe" << endl;
+    cout << "2- Four-in-a-row" << endl;
+    cout << "3- 5 x 5 Tic Tac Toe" << endl;
+    cout << "4- Word Tic-tac-toe" << endl;
+    cout << "5- Numerical Tic-Tac-Toe" << endl;
+    cout << "6- Misere Tic Tac Toe" << endl;
+    cout << "7- 4 x 4 Tic-Tac-Toe" << endl;
+    cout << "8- Ultimate Tic Tac Toe" << endl;
+    cin >> n;
+    if (n == '0') {
+        cout << "Good bye" << endl;
+        return 0;
+    } else if (n == '1') {
+        game1();
+        main();
+
+    } else if (n == '2') {
+        // game2();
+        main();
+
+    } else if (n == '3') {
+        //game3();
+        main();
+
+    } else if (n == '4') {
+        //game4();
+        main();
+
+    } else if (n == '5') {
+        //game5();
+        main();
+
+    } else if (n == '6') {
+        //game6();
+        main();
+
+    } else if (n == '7') {
+        //game7();
+        main();
+
+    } else if (n == '8') {
+        //game8();
+        main();
+
+    } else {
+        cout << "Invalid choice " << endl;
+        main();
+    }
+
+    return 0;
+}
+
+
+
