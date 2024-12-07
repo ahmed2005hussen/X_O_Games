a920fe890512647bd707c667d7efabb7bfb504e2
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2024-12-07 23:24:25 +0200

    Add files via upload

---
+++ game7.h
@@ -0,0 +1,382 @@
+//
+// Created by ahmed on 06/12/2024.
+//
+#include"BoardGame_Classes.h"
+
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include<algorithm>
+#include <cstdlib>
+#include <ctime>
+#include<fstream>
+
+#ifndef XO_GAME7_H
+#define XO_GAME7_H
+
+using namespace std;
+
+
+template<typename T>
+class board7 : public Board<T> {
+public:
+    board7();
+
+    bool update_board(int x, int y, T symbol);
+
+    void display_board();
+
+    bool is_win();
+
+    bool is_draw();
+
+    bool game_is_over();
+};
+
+template<typename T>
+class player_7 : public Player<T> {
+public:
+    player_7(string name, T symbol);
+
+    void getmove(int &x, int &y);
+};
+
+
+template<typename T>
+class random_player_7 : public RandomPlayer<T> {
+public:
+    random_player_7(T symbol);
+
+    void getmove(int &x, int &y);
+};
+
+
+// --------Implementation--------
+
+template<typename T>
+board7<T>::board7() {
+    this->rows = this->columns = 4;
+    this->board = new char *[this->rows];
+    bool flag = true;
+    for (int i = 0; i < this->rows; i++) {
+        this->board[i] = new char[this->columns];
+        for (int j = 0; j < this->columns; j++) {
+            if (i == 0) {
+                if (flag) {
+                    this->board[i][j] = 'O';
+                    flag = !flag;
+                } else {
+                    this->board[i][j] = 'X';
+                    if (j != 3) { flag = !flag; }
+                }
+            } else if (i == 3) {
+                if (flag) {
+                    this->board[i][j] = 'O';
+                    flag = !flag;
+                } else {
+                    this->board[i][j] = 'X';
+                    flag = !flag;
+                }
+            } else {
+                this->board[i][j] = ' ';
+            }
+        }
+    }
+    this->n_moves = 0;
+}
+
+template<typename T>
+bool board7<T>::update_board(int x, int y, T symbol) {
+    bool flag = false;
+
+    int pos1 = x;
+    int pos2 = y ;
+
+    if (x >= 0 && x < 4 && y >= 0 && y < 4 ) {
+        int n;
+
+        if (x == 0 && y == 0) {
+
+            if (this->board[x][y + 1] ==' ' && (this->board[x + 1][y] == ' ')) {
+                cout << "do you want move it " << endl;
+                cout << "1 - right " << endl;
+                cout << "2 - down " << endl;
+                cin >> n;
+                if (n == 1) {
+                    x = x;
+                    y = y + 1;
+                } else if (n == 2) {
+                    x = x + 1;
+                    y = y;
+                }
+            } else if ((this->board[x][y + 1]==' ')) {
+                cout << "it will right move " << endl;
+
+                x = x;
+                y = y + 1;
+            } else if ((this->board[x + 1][y]==' ')) {
+                cout << "it will down move " << endl;
+                x = x + 1;
+                y = y;
+
+            } else {
+                cout << "we can't move it " << endl;
+                return false;
+            }
+
+        } else if (x == 0 && y == 3) {
+            if ((this->board[x][y + 1]==' ') && (this->board[x + 1][y]==' ')) {
+                cout << "do you want move it " << endl;
+                cout << "1 - left " << endl;
+                cout << "2 - down " << endl;
+                cin >> n;
+                if (n == 1) {
+                    x = x;
+                    y = y - 1;
+                } else if (n == 2) {
+                    x = x + 1;
+                    y = y;
+                }
+            } else if ((this->board[x][y + 1]==' ')) {
+                cout << "it will left move " << endl;
+                x = x;
+                y = y - 1;
+            } else if ((this->board[x + 1][y]==' ')) {
+                cout << "it will down move " << endl;
+                x = x + 1;
+                y = y;
+            } else {
+                cout << "we can't move it " << endl;
+                return false;
+            }
+
+        } else if (x == 0) {
+            cout << "do you want move it " << endl;
+            cout << "1 - right " << endl;
+            cout << "2 - left " << endl;
+            cout << "3 - down " << endl;
+            cin >> n;
+            if (n == 1) {
+                x = x;
+                y = y + 1;
+            } else if (n == 2) {
+                x = x;
+                y = y - 1;
+            } else if (n == 3) {
+                x = x + 1;
+                y = y;
+            } else { return false; }
+        } else if ((x == 1 && y == 0) || (x == 2 && y == 0)) {
+            cout << "do you want move it " << endl;
+            cout << "1 - right " << endl;
+            cout << "2 - up " << endl;
+            cout << "3 - down " << endl;
+            cin >> n;
+            if (n == 1) {
+                x = x;
+                y = y + 1;
+            } else if (n == 2) {
+                x = x - 1;
+                y = y;
+            } else if (n == 3) {
+                x = x + 1;
+                y = y;
+            } else { return false; }
+        } else if ((x == 1 && y == 3) || (x == 2 && y == 3)) {
+            cout << "do you want move it " << endl;
+            cout << "1 - left " << endl;
+            cout << "2 - up " << endl;
+            cout << "3 - down " << endl;
+            cin >> n;
+            if (n == 1) {
+                x = x;
+                y = y - 1;
+            } else if (n == 2) {
+                x = x - 1;
+                y = y;
+            } else if (n == 3) {
+                x = x + 1;
+                y = y;
+            } else { return false; }
+        } else if (x == 1 || x == 2) {
+            cout << "do you want move it " << endl;
+            cout << "1 - right " << endl;
+            cout << "2 - left " << endl;
+            cout << "3 - up " << endl;
+            cout << "4 - down " << endl;
+            cin >> n;
+            if (n == 1) {
+                x = x;
+                y = y + 1;
+            } else if (n == 2) {
+                x = x;
+                y = y - 1;
+            } else if (n == 3) {
+                x = x - 1;
+                y = y;
+            } else if (n == 4) {
+                x = x + 1;
+                y = y;
+            } else { return false; }
+        } else if (x == 3 && y == 0) {
+            cout << "do you want move it " << endl;
+            cout << "1 - right " << endl;
+            cout << "2 - up " << endl;
+            cin >> n;
+            if (n == 1) {
+                x = x;
+                y = y + 1;
+            } else if (n == 2) {
+                x = x - 1;
+                y = y;
+            } else { return false; }
+        } else if (x == 3 && y == 3) {
+            cout << "do you want move it " << endl;
+            cout << "1 - left " << endl;
+            cout << "2 - up " << endl;
+            cin >> n;
+            if (n == 1) {
+                x = x;
+                y = y - 1;
+            } else if (n == 2) {
+                x = x - 1;
+                y = y;
+            } else { return false; }
+        } else if (x == 3) {
+            cout << "do you want move it " << endl;
+            cout << "1 - right " << endl;
+            cout << "2 - left " << endl;
+            cout << "3 - up " << endl;
+            cin >> n;
+            if (n == 1) {
+                x = x;
+                y = y + 1;
+            } else if (n == 2) {
+                x = x;
+                y = y - 1;
+            } else if (n == 3) {
+                x = x - 1;
+                y = y;
+            } else { return false; }
+        }
+        this->board[x][y] = toupper(symbol);
+        this->board[pos1][pos2] = ' ';
+        this->n_moves++;
+        return true;
+    }
+
+
+
+    return false;
+}
+
+template<typename T>
+void board7<T>::display_board() {
+
+    for (int i = 0; i < this->rows; i++) {
+        cout << "\n| ";
+        for (int j = 0; j < this->columns; j++) {
+            cout << setw(2) << this->board[i][j] << " |";
+        }
+        cout << "\n-------------------------";
+    }
+    cout << endl;
+}
+
+template<typename T>
+
+
+bool board7<T>::is_win() {
+
+
+        for (int i = 0; i < this->rows; i++) {
+            for (int j = 0; j <= this->columns - 4; j++) {
+                if (this->board[i][j] != ' ' &&
+                    this->board[i][j] == this->board[i][j + 1] &&
+                    this->board[i][j] == this->board[i][j + 2] &&
+                    this->board[i][j] == this->board[i][j + 3]) {
+                    return true;
+                }
+            }
+        }
+
+
+        for (int j = 0; j < this->columns; j++) {
+            for (int i = 0; i <= this->rows - 4; i++) {
+                if (this->board[i][j] != ' ' &&
+                    this->board[i][j] == this->board[i + 1][j] &&
+                    this->board[i][j] == this->board[i + 2][j] &&
+                    this->board[i][j] == this->board[i + 3][j]) {
+                    return true;
+                }
+            }
+        }
+
+        for (int i = 0; i <= this->rows - 4; i++) {
+            for (int j = 0; j <= this->columns - 4; j++) {
+                if (this->board[i][j] != ' ' &&
+                    this->board[i][j] == this->board[i + 1][j + 1] &&
+                    this->board[i][j] == this->board[i + 2][j + 2] &&
+                    this->board[i][j] == this->board[i + 3][j + 3]) {
+                    return true;
+                }
+            }
+        }
+
+        for (int i = 0; i <= this->rows - 4; i++) {
+            for (int j = 3; j < this->columns; j++) {
+                if (this->board[i][j] != ' ' &&
+                    this->board[i][j] == this->board[i + 1][j - 1] &&
+                    this->board[i][j] == this->board[i + 2][j - 2] &&
+                    this->board[i][j] == this->board[i + 3][j - 3]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+template<typename T>
+bool board7<T>::is_draw() {
+    return false;
+}
+
+template<typename T>
+bool board7<T>::game_is_over() {
+    return is_win() ;
+}
+
+
+template<typename T>
+player_7<T>::player_7(std::string name, T symbol) : Player<T>(name, symbol) {}
+
+
+template<typename T>
+void player_7<T>::getmove(int &x, int &y) {
+    cout << "\n" << this->name << ", enter the position for your token do you want to move : ";
+
+    cin >> x >> y;
+
+
+}
+
+//----------------------------------------
+
+template<typename T>
+random_player_7<T>::random_player_7(T symbol): RandomPlayer<T>(symbol) {}
+
+template<typename T>
+void random_player_7<T>::getmove(int &x, int &y) {
+    srand(static_cast<unsigned int>(time(0)));
+    char randomChar = 'A' + (rand() % 26);
+    x = rand() % 3;
+    y = rand() % 3;
+    this->symbol = randomChar;
+}
+
+
+#endif //XO_GAME7_H
