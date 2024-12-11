0b2c42bc0a2dcdf6041b905cbf27653ceec0c60e
Menna-AbdElGawad <157136287+Menna-AbdElGawad@users.noreply.github.com>
2024-12-12 00:27:07 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include " Pyramic_Tic-Tac-Toe.h"
 #include "Word_Tic-tac-toe.h"
 #include "Four_In_Row.h"
+#include "Num_Tic_Tac_Toe.h"
 using namespace std;
 
 void game1() {
@@ -189,7 +190,71 @@ void game4(){
     }
 
 }
-void game5(){}
+
+void game5()
+{
+    char choice;
+    Player<char>* players[2];
+    Num_Board<char>* B = new Num_Board<char>();
+    string playerXName, player2Name;
+
+
+    // Set up player 1
+    cout << "Enter Player X name: ";
+    cin >> playerXName;
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    while (true) {
+        cin >> choice;
+
+        if (choice == '1') {
+            players[0] = new Num_Player<char>(playerXName, 1);
+            break;
+        }
+        else if (choice == '2') {
+            players[0] = new Num_Random_Player<char>(1);
+            break;
+        }
+        else {
+            cout << "Invalid choice for Player 1. enter valid choice \n";
+        }
+    }
+    // Set up player 2
+    cout << "Enter Player 2 name: ";
+    cin >> player2Name;
+    cout << "Choose Player 2 type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+
+    while (true) {
+        cin >> choice;
+
+        if (choice == '1') {
+            players[1] = new Num_Player<char>(player2Name, 2);
+            break;
+        }
+        else if (choice == '2') {
+            players[1] = new Num_Random_Player<char>(2);
+            break;
+        }
+        else {
+            cout << "Invalid choice for Player 2. enter valid choice.\n";
+        }
+    }
+
+    // Create the game manager and run the game
+    GameManager<char> pyramid_game(B, players);
+    pyramid_game.run();
+
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+}
+
+
 void game6(){}
 void game7(){
 
@@ -282,7 +347,7 @@ int main() {
         main();
 
     } else if (n == '5') {
-        //game5();
+        game5();
         main();
 
     } else if (n == '6') {
@@ -290,7 +355,7 @@ int main() {
         main();
 
     } else if (n == '7') {
-       game7();
+        game7();
         main();
 
     } else if (n == '8') {
